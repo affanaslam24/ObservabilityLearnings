8667fbbff68da8ccdfa0a87602b274834b87d51d
AffanA-1 <mdaffanaslam@cloudkaptan.com>
2026-09-16 17:04:01 +0530

    Custom Label included for Metric

--- appObsNode/server.js
+++ appObsNode/server.js
@@ -3,6 +3,10 @@ const express = require("express")
 const client = require("prom-client")
 const {heavyTask} = require("./util")
 
+// FOR STEP4
+const responseTime = require("response-time");
+
+
 const app = express()
 const PORT = process.env.PORT || 3000
 
@@ -11,7 +15,7 @@ app.use(express.json())
 // STEP2
 const collectDefaultMetrics = client.collectDefaultMetrics;
 collectDefaultMetrics({register: client.register})
-console.log(client.register);
+
 
 // STEP3
 app.get("/metrics", async(req, res)=> {
@@ -20,8 +24,31 @@ app.get("/metrics", async(req, res)=> {
     res.send(metrics)
 })
 
-// // STEP4: Creating own Client
-// const reqResTime = new client.Histogram
+// STEP4: Creating own Client
+const reqResTime = new client.Histogram({
+    name: "http_express_req_res_time",
+    help: "THis tells us how much time is taken by req and res",
+    labelNames: ["method", "route", "status_code"],
+    buckets: [1, 50, 100, 200, 400, 500, 800, 1000, 2000]
+})
+// add the custom metric to the middleware of the app
+app.use(
+    responseTime((req, res, time) => {
+        totalReqCounter.inc()
+        reqResTime.labels({
+            method: req.method,
+            route: req.url,
+            status_code: res.statusCode
+        })
+        .observe(time);
+    })
+)
+
+// STEP5: one more Custom LAbel
+const totalReqCounter = new client.Counter({
+    name: 'total_req',
+    help: 'tells total number of req'
+})
 
 app.get("/", (req, res)=> {
     res.json({message: "Backend is running!"})
--- appObsNode/test.js
+++ appObsNode/test.js
@@ -1,6 +1,6 @@
 async function testing(){
     while(true){
-        fetch("http://localhost:3000/slow")
+        await fetch("http://localhost:3000/slow")
     }
 }
 testing()
